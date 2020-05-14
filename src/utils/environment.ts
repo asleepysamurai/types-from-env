4d30950cf87cd2f5a49653963f85aa134a86abf7
Balaganesh Damodaran <asleepysamurai@gmail.com>
2020-05-14 21:38:58 +0200

    Implemented - Read config from <process.env.NODE_ENV>.env, merge to default.env and export via Environment singleton

---
+++ src/utils/environment.ts
@@ -0,0 +1,30 @@
+/**
+ * Environment setup
+ *
+ * Uses dotenv to parse different env files based on process.env.NODE_ENV
+ * Also parses a default.env file and merges it to the <env>.env
+ */
+
+import dotenv from 'dotenv';
+import * as path from 'path';
+
+class Environment {
+  constructor() {
+    const defaultEnvironment = 'development';
+    const environmentFileDir = (env: string): string =>
+      path.resolve(__dirname, `../../config/${env}.env`);
+
+    dotenv.config({
+      path: environmentFileDir(process.env.NODE_ENV || defaultEnvironment),
+    });
+    dotenv.config({
+      path: environmentFileDir('default'),
+    });
+  }
+
+  get(key: string): any {
+    return process.env[key];
+  }
+}
+
+export const env = new Environment();
